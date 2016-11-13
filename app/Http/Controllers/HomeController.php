d2a40a177cfeeb24f86f9c197e4412cc84bb4288
migoran78 <mi.goran@hotmail.com>
2016-11-13 18:33:36 +0100

    13/11-3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class HomeController extends Controller
 {
     /**
@@ -21,8 +22,32 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+
+        //--  WRITE SESSION DATA
+        session(['edwin'=>'teacher']);
+        $request->session()->put(['goran'=>'administrator']);
+
+
+        //--  DELETE SESSION DATA
+//        $request->session()->flush();     //brise i podatke o logovanom korisniku
+        $request->session()->forget('goran');
+        $request->session()->forget('edwin');
+
+
+        //---- FLASH DATA --
+        $request->session()->flash('message', 'Post has been created');
+
+//        $request->session()->reflash();
+//        $request->session()->keep('message');
+
+
+        //--  READ SESSION DATA
+//        echo   $request->session()->get('goran');
+        return $request->session()->all();
+
+
         return view('home');
     }
 }
