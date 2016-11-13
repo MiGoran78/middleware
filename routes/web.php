ba369f963a3f25270565cf75e68af6f2ca283a7e
migoran78 <mi.goran@hotmail.com>
2016-11-13 15:00:45 +0100

    13/11-2

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,20 @@
 */
 
 Route::get('/', function () {
+
     return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+
+
+Route::get('/admin/user/roles', ['middleware'=>['IsAdmin', 'role', 'auth', 'web'], function (){
+
+    return "Middleware role";
+}]);
+
+
+Route::get('/admin', 'AdminController@index');
